948fc79abd9ad4ae4af602d4f203d2d9055dc578
metanivek <metanivek@gmail.com>
2023-04-24 09:24:21 -0400

    Misc tweaks

--- init.el
+++ init.el
@@ -121,7 +121,8 @@
        ;;erlang            ; an elegant language for a more civilized age
        ;;ess               ; emacs speaks statistics
        ;;faust             ; dsp, but you get to keep your soul
-       ;;fsharp            ; ML stands for Microsoft's Language
+       (fsharp             ; ML stands for Microsoft's Language
+        +lsp)
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        (go
--- packages.el
+++ packages.el
@@ -14,6 +14,7 @@
 (package! company-glsl)
 (package! glsl-mode)
 (package! pacfiles-mode)
+(package! sqlite)
 (package! gnuplot)
 
 ;;; debugging/utility packages
